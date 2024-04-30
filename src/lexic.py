0bc00071bea9722e28d92da04b12a9ea32f98bfb
ryan-leal <ryanxd178@gmail.com>
2024-04-30 18:59:10 -0300

    tokenization and posTAG - Spacy

--- src/lexic.py
+++ src/lexic.py
@@ -1,2 +1,14 @@
-def lexic():
+import spacy
+
+nlp = spacy.load("pt_core_news_sm")
+
+def tokenize(inputText):
+    doc = nlp(inputText)
+    for token in doc:
+         print(f"{token.text}: {token.pos_}")
+         
+def lexic(inputText):
     print('[LEX] Returning token table')
+    tokenize(inputText)
+
+    
--- src/main.py
+++ src/main.py
@@ -3,8 +3,9 @@ import syntatic
 import semantic
 
 def main():
+    inputText = 'A paraíba é o estado do Nordeste que mais ampliou sua rede obstetrícia' 
     print('[MAIN] Calling Lexic...')
-    lexic.lexic()
+    lexic.lexic(inputText)
     print('[MAIN] Calling Syntatic...')
     syntatic.syntatic()
     print('[MAIN] Calling Semantic...')
